092ce90eaf71777b417fa390d9f5a9ac106045c2
Lordofrealms <djwilsonou@gmail.com>
2026-08-21 18:38:30 -0500

    Configure Precision Location Android app

---
+++ precision-location-android/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.lordofrealms.precisionlocation"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.lordofrealms.precisionlocation"
+        minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+
+        externalNativeBuild {
+            cmake {
+                arguments += listOf("-DANDROID_STL=c++_static")
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
