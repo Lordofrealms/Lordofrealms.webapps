132be2f663e2893b0626d1648ac56427f1512e61
Lordofrealms <djwilsonou@gmail.com>
2026-08-22 09:18:18 -0500

    Bump Precision Location to v0.4.3

--- precision-location-android/app/build.gradle.kts
+++ precision-location-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.lordofrealms.precisionlocation"
         minSdk = 31
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.4.2"
+        versionCode = 9
+        versionName = "0.4.3"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
