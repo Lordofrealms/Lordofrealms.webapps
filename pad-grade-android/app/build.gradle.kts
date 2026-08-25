34086c1e924ab939231fca7dc21d2b46265d96da
Lordofrealms <djwilsonou@gmail.com>
2026-08-25 13:04:20 -0500

    Bump Pad Grade DEV to v0.6.9

--- pad-grade-android/app/build.gradle.kts
+++ pad-grade-android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lordofrealms.padgrade.dev"
         minSdk = 31
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.6.8"
+        versionCode = 36
+        versionName = "0.6.9"
     }
 
     buildTypes {
