9b0a3aac667bc53593449464afa9b0e4939b7b44
Lordofrealms <djwilsonou@gmail.com>
2026-08-29 17:39:10 -0500

    Pad Grade Android v0.9.7 DEV

--- pad-grade-android/app/build.gradle.kts
+++ pad-grade-android/app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         manifestPlaceholders["appLabel"] = if (padGradeDevBuild) "Pad Grade DEV" else "Pad Grade"
         minSdk = 31
         targetSdk = 36
-        versionCode = 63
-        versionName = "0.9.6"
+        versionCode = 64
+        versionName = "0.9.7"
     }
 
     buildTypes {
